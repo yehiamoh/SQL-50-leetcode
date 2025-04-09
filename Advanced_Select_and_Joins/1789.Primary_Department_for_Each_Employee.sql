8effa79d3967817527e8d5da3a83f532922a4ea2
Yehia Mohamed <yehiamohamed915@yahoo.com>
2025-04-09 13:11:51 +0200

    1789. Primary Department for Each Employee

---
+++ Advanced_Select_and_Joins/1789.Primary_Department_for_Each_Employee.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+select employee_id,department_id
+from Employee
+where primary_flag ='Y' 
+or 
+employee_id in(
+    select employee_id 
+    from Employee
+    group by employee_id
+    having count(department_id)=1
+)
